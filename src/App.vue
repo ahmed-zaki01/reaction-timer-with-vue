f4e35eb85467873273fd1ef6033c857a7ef14c93
ahmed_zaki0263 <ahmed.zaki0263@gmail.com>
2021-05-11 15:41:14 +0200

    finish the reaction time game

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,34 @@
 <template>
   <h1>Reaction Timer</h1>
   <button id="play" @click="start" :disabled="isPlaying">Play</button>
-  <block v-if="isPlaying" :delay="delay" />
+  <block v-if="isPlaying" :delay="delay" @end="endGame" />
+  <result v-if="showResult" :score="score" />
 </template>
 
 <script>
 import Block from "./components/Block.vue";
+import Result from "./components/Result.vue";
 export default {
   name: "App",
-  components: { Block },
+  components: { Block, Result },
   data() {
     return {
       isPlaying: false,
       delay: null,
+      score: null,
+      showResult: false,
     };
   },
   methods: {
     start() {
       this.delay = 2000 + Math.random() * 5000;
       this.isPlaying = true;
+      this.showResult = false;
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime;
+      this.isPlaying = false;
+      this.showResult = true;
     },
   },
 };
@@ -34,7 +44,18 @@ export default {
   margin-top: 60px;
 }
 button#play {
-  padding: 5px 10px;
+  padding: 8px 16px;
+  font-size: 1em;
+  letter-spacing: 1px;
+  background: #0faf87;
+  color: #e6e6e6;
+  border: none;
+  border-radius: 5px;
   cursor: pointer;
+}
+
+button#play[disabled] {
+  cursor: not-allowed;
+  opacity: 0.6;
 }
 </style>
---
+++ src/components/Result.vue
@@ -0,0 +1,51 @@
+<template>
+  <p>Reaction Time - {{ score }} ms</p>
+  <p v-if="rank" id="rank" :class="classObject">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+  props: ["score"],
+  data() {
+    return {
+      rank: null,
+    };
+  },
+  mounted() {
+    if (this.score < 250) {
+      this.rank = "Ninja Fingers";
+    } else if (this.score < 400) {
+      this.rank = "Rapid Reflexes";
+    } else {
+      this.rank = "Snail pace...";
+    }
+  },
+
+  computed: {
+    classObject() {
+      return {
+        ninja: this.score < 250,
+        rapid: this.score >= 250 && this.score < 400,
+        snail: this.score >= 400,
+      };
+    },
+  },
+};
+</script>
+
+<style>
+p#rank {
+  font-size: 1.4em;
+  font-weight: bold;
+}
+
+p.ninja {
+  color: #0faf87;
+}
+p.rapid {
+  color: turquoise;
+}
+p.snail {
+  color: orangered;
+}
+</style>
